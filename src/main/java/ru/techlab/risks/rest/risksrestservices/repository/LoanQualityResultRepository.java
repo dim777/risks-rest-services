500b4a58a605a22f93bfa655e8a71c4e2a76f8a9
dim777 <dim777@ya.ru>
2017-10-05 08:30:37 +0300

    Added @Cacheable to repo

--- src/main/java/ru/techlab/risks/rest/risksrestservices/repository/LoanQualityResultRepository.java
+++ src/main/java/ru/techlab/risks/rest/risksrestservices/repository/LoanQualityResultRepository.java
@@ -1,5 +1,6 @@
 package ru.techlab.risks.rest.risksrestservices.repository;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.cassandra.repository.CassandraRepository;
 import org.springframework.data.cassandra.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -11,6 +12,7 @@ import java.util.stream.Stream;
  * Created by dim777 on 05.10.17.
  */
 @Repository
+@Cacheable("loanQualityResultRepository")
 public interface LoanQualityResultRepository extends CassandraRepository<LoanQualityResult> {
     @Query("SELECT * FROM SRRU WHERE RRABD = ?0 AND RRAND = ?1 AND RRASD = ?2")
     Stream<LoanQualityResult> findSimpleDelayByLoan(String branch, String loanAccountNumber, String loanAccountSuffix);
