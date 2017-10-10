87f716726002bfd721ba34f5716e0b2be6dba333
dim777 <dim777@ya.ru>
2017-10-10 20:08:22 +0300

    Modify Models

--- src/main/java/ru/techlab/risks/rest/risksrestservices/model/LoanQualityResult.java
+++ src/main/java/ru/techlab/risks/rest/risksrestservices/model/LoanQualityResult.java
@@ -8,32 +8,45 @@ import org.springframework.data.cassandra.mapping.Table;
 import ru.xegex.risks.libs.model.quality.LoanQualityCategory;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.Optional;
 
 /**
  * Created by dim777 on 05.10.17.
  */
-@Table("KKS_RESULTS")
+@Table("kks_results")
 @Data
 public class LoanQualityResult implements Serializable {
     private static final long serialVersionUID = 3375159358757648792L;
 
-    @PrimaryKeyColumn(name = "branchId", ordinal = 3, type = PrimaryKeyType.PARTITIONED)
+    @PrimaryKeyColumn(name = "branch_id", ordinal = 3, type = PrimaryKeyType.PARTITIONED)
     private String branch;
 
-    @PrimaryKeyColumn(name = "customerId", ordinal = 2, type = PrimaryKeyType.CLUSTERED)
+    @PrimaryKeyColumn(name = "customer_id", ordinal = 2, type = PrimaryKeyType.CLUSTERED)
     private String loanAccountNumber;
 
     @PrimaryKeyColumn(name = "suffix", ordinal = 1, type = PrimaryKeyType.CLUSTERED)
     private String loanAccountSuffix;
 
-    @Column("customerName")
-    private Optional<String> customerName;
+    @Column("customer_name")
+    private String customerName;
+
+    @Column("dddt1")
+    private String startDate;
+
+    @Column("balance")
+    private BigDecimal balance;
 
     @Column("kks0")
-    private Optional<LoanQualityCategory> loanQualityCategory;
+    private Integer loanQualityCategory;
 
     @Column("kks1")
-    private Optional<LoanQualityCategory> loanQualityCategoryForAllCustomerLoans;
+    private Integer loanQualityCategoryForAllCustomerLoans;
+
+    @Column("interest_rate0")
+    private Double interestRate;
+
+    @Column("interest_rate1")
+    private Double interestRateAll;
 
 }
